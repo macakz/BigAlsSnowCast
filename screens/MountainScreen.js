b4c51c3fd0eda665a161b6fff172dd0bc87a09ea
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-21 17:51:16 +1200

    able to pull in weather id

--- screens/MountainScreen.js
+++ screens/MountainScreen.js
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, } from 'react-native';
+import { Button, StyleSheet, Text, } from 'react-native';
+import skifields from '../data/skifields.json'
 
 const MountainScreen = ({ route, navigation }) => {
+
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
         },
-    });
+    })
 
-    const data = route.params;
+    const mountain = route.params
+    const setMountain = mountain.value
+    const [findMountainId, setFindMountainId] = useState('hello')
+
+    const matchMountainId = () => {
+        let match = skifields.find(field => field.name === setMountain);
+        setFindMountainId(match.weatherId);
+    }
 
     return (
         <>
-            <Text>{data.value}</Text>
+            <Text style={styles.title}>{setMountain}</Text>
+            <Text>{findMountainId}</Text>
+            <Button title="mountain" onPress={() => matchMountainId()} />
         </>
     )
 }
