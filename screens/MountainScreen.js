62e03606cff24fa9ef8f441daa2fff3f9b9d38c9
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-22 18:33:21 +1200

    can access snow data, will have an option to change the height at which you view the forecast

--- screens/MountainScreen.js
+++ screens/MountainScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, Button, StyleSheet, Text, } from 'react-native';
+import { ActivityIndicator, Button, SafeAreaView, ScrollView, StyleSheet, Text, } from 'react-native';
 import skifields from '../data/skifields.json'
 import { app_id, app_key } from '../config/weatherKeys'
 import axios from 'axios'
 import { useFocusEffect } from '@react-navigation/native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
 
 const MountainScreen = ({ route, navigation }) => {
 
@@ -23,6 +25,13 @@ const MountainScreen = ({ route, navigation }) => {
     }
     const [mountainForecastData, setMountainForecastData] = useState([])
     const [mountainIsReady, setMountainIsReady] = useState(false)
+
+    const [open, setOpen] = useState(false);
+    const [heightValue, setHeightValue] = useState("mid");
+    const items = [{ label: "base", value: "base" },
+    { label: "mid", value: "mid" },
+    { label: "upper", value: "upper" }]
+
 
     useEffect(() => {
         mountainForecast()
@@ -49,8 +58,33 @@ const MountainScreen = ({ route, navigation }) => {
     }
     return (
         <>
-            <Text>hi</Text>
-            <Text>{mountainForecastData[0].snow_mm}</Text>
+            <SafeAreaView>
+                <ScrollView>
+                    <DropDownPicker
+                        listMode="MODAL"
+                        modalContentContainerStyle={styles.modalContentContainerStyle}
+                        style={styles.dropDownPicker}
+                        placeholder="Choose your mountain"
+                        open={open}
+                        value={heightValue}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setHeightValue}
+                    />
+                    {mountainForecastData.map((info) => {
+                        return (
+                            <>
+                                <Text>{info.date}</Text>
+                                <Text>{info.time}</Text>
+                                <Text>{info.mid.freshsnow_cm}cm</Text>
+                                <Text>{info.rain_mm}</Text>
+
+                            </>
+                        )
+                    })
+                    }
+                </ScrollView>
+            </SafeAreaView>
         </>
     )
 }
