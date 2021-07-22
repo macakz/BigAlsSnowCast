a64edbf520d57d1cc09402993e6683480b84de6d
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-22 15:42:59 +1200

    working on displaying api info

--- screens/MountainScreen.js
+++ screens/MountainScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, } from 'react-native';
 import skifields from '../data/skifields.json'
 import { app_id, app_key } from '../config/weatherKeys'
@@ -14,12 +14,14 @@ const MountainScreen = ({ route, navigation }) => {
     })
 
     const mountain = route.params.value
-    const [mountainId, setMountainId] = useState('')
+    const [mountainId, setMountainId] = useState()
     const matchMountainId = () => {
         let match = skifields.find(field => field.name === mountain);
         setMountainId(match.weatherId);
     }
-    const [mountainForecastData, setMountainForecastData] = useState('')
+    const [mountainForecastData, setMountainForecastData] = useState([])
+
+
     const mountainForecast = () => {
         axios.get(`https://api.weatherunlocked.com/api/resortforecast/${mountainId}?app_id=${app_id}&app_key=${app_key}`)
             .then(function (response) {
@@ -36,7 +38,7 @@ const MountainScreen = ({ route, navigation }) => {
             <Text>{mountainId}</Text>
             <Button title="mountain" onPress={() => matchMountainId()} />
             <Button title="forecast" onPress={() => mountainForecast()} />
-
+            <Text>{mountainForecastData[17].snow_mm}</Text>
         </>
     )
 }
