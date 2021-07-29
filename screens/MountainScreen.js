ff28ced46c60e08e29d3c5c6e9058f21b785e34e
Alexander McKerrow <mckerrow4@gmail.com>
2021-07-29 18:15:25 +1200

    style

--- screens/MountainScreen.js
+++ screens/MountainScreen.js
@@ -1,6 +1,6 @@
 //react
 import React, { useState, useEffect, useLayoutEffect } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image, Linking, TouchableOpacity } from 'react-native'
+import { SafeAreaView, ScrollView, Text, View, Image, Linking, TouchableOpacity } from 'react-native'
 
 //config
 import { app_id, app_key } from '../config/weatherKeys'
@@ -90,7 +90,7 @@ const MountainScreen = ({ route, navigation }) => {
                         <Col>
                             <View style={styles.dropDownPickerContainerLeft}>
                                 <DropDownPicker
-                                    listMode="DEFAULT"
+                                    listMode="MODAL"
                                     style={styles.dropDownPicker}
                                     modalContentContainerStyle={styles.modalContentContainerStyle}
                                     labelStyle={styles.dropDownPickerLabel}
@@ -108,7 +108,7 @@ const MountainScreen = ({ route, navigation }) => {
                                     dropDownContainerStyle={{
                                         fontFamily: 'CrimsonText-Regular'
                                     }}
-                                    listMode="DEFAULT"
+                                    listMode="MODAL"
                                     style={styles.dropDownPicker}
                                     modalContentContainerStyle={styles.modalContentContainerStyle}
                                     labelStyle={styles.dropDownPickerLabel}
@@ -121,40 +121,61 @@ const MountainScreen = ({ route, navigation }) => {
                             </View>
                         </Col>
                     </Grid>
-                    <View style={{ zIndex: 1000, }}>
-                        {mountainForecastData.map((data) => {
-                            const icon = data.upper.wx_icon.replace(".gif", "")
-                            const getDayOfWeek = value => {
-                                const [dd, mm, yyyy] = value.split('/'),
-                                    date = new Date(yyyy, mm - 1, dd)
-                                return date.toLocaleDateString('en-US', { weekday: 'long' })
-                            }
+                    {mountainForecastData.map((data) => {
+                        const icon = data.upper.wx_icon.replace(".gif", "")
+                        const getDayOfWeek = value => {
+                            const [dd, mm, yyyy] = value.split('/'),
+                                date = new Date(yyyy, mm - 1, dd)
+                            return date.toLocaleDateString('en-US', { weekday: 'long' })
+                        }
 
-                            return (
-                                <>
-                                    <View style={styles.dataContainer}>
-                                        <Grid>
-                                            <Col size={65}>
-                                                <View style={styles.dataRowContainer}>
-                                                    <Text style={styles.dataTitle}>Date: </Text>
-                                                    <Text style={styles.dataContent}>{getDayOfWeek(data.date)} - {data.date}</Text>
-                                                </View>
-                                                <View style={styles.dataRowContainer}>
-                                                    <Text style={styles.dataTitle}>Time ({hourValue} hourly intervals): </Text>
-                                                    <Text style={styles.dataContent}>{data.time}</Text>
-                                                </View>
-                                                <View style={styles.dataRowContainer}>
-                                                    <Text style={styles.dataTitle}>Freezing Level: </Text>
-                                                    <Text style={styles.dataContent}>{data.frzglvl_avg_m}m</Text>
-                                                </View>
-                                                {
-                                                    //base on mountain
-                                                    heightValue === "Base"
+                        return (
+                            <>
+                                <View style={styles.dataContainer}>
+                                    <Grid>
+                                        <Col size={65}>
+                                            <View style={styles.dataRowContainer}>
+                                                <Text style={styles.dataTitle}>Date: </Text>
+                                                <Text style={styles.dataContent}>{getDayOfWeek(data.date)} - {data.date}</Text>
+                                            </View>
+                                            <View style={styles.dataRowContainer}>
+                                                <Text style={styles.dataTitle}>Time ({hourValue} hourly intervals): </Text>
+                                                <Text style={styles.dataContent}>{data.time}</Text>
+                                            </View>
+                                            <View style={styles.dataRowContainer}>
+                                                <Text style={styles.dataTitle}>Freezing Level: </Text>
+                                                <Text style={styles.dataContent}>{data.frzglvl_avg_m}m</Text>
+                                            </View>
+                                            {
+                                                //base on mountain
+                                                heightValue === "Base"
+                                                    ?
+                                                    <>
+                                                        <View style={styles.dataRowContainer}>
+                                                            <Text style={styles.dataTitle}>Snow: </Text>
+                                                            <Text style={styles.dataContent}>{data.base.freshsnow_cm.toFixed(2)}cm</Text>
+                                                        </View>
+                                                        <View style={styles.dataRowContainer}>
+                                                            <Text style={styles.dataTitle}>Temperature: </Text>
+                                                            <Text style={styles.dataContent}>{data.base.temp_c}°C</Text>
+                                                        </View>
+                                                        <View style={styles.dataRowContainer}>
+                                                            <Text style={styles.dataTitle}>Wind Direction and Speed: </Text>
+                                                            <Text style={styles.dataContent}>{data.base.winddir_compass} {data.base.windspd_kmh}km/h</Text>
+                                                        </View>
+                                                        <View style={styles.dataRowContainer}>
+                                                            <Text style={styles.dataTitle}>Forecast: </Text>
+                                                            <Text style={styles.dataContent}>{data.base.wx_desc}</Text>
+                                                        </View>
+                                                    </>
+                                                    :
+                                                    //mid mountain
+                                                    heightValue === "Mid"
                                                         ?
                                                         <>
                                                             <View style={styles.dataRowContainer}>
                                                                 <Text style={styles.dataTitle}>Snow: </Text>
-                                                                <Text style={styles.dataContent}>{data.base.freshsnow_cm.toFixed(2)}cm</Text>
+                                                                <Text style={styles.dataContent}>{data.mid.freshsnow_cm.toFixed(2)}cm</Text>
                                                             </View>
                                                             <View style={styles.dataRowContainer}>
                                                                 <Text style={styles.dataTitle}>Temperature: </Text>
@@ -162,74 +183,51 @@ const MountainScreen = ({ route, navigation }) => {
                                                             </View>
                                                             <View style={styles.dataRowContainer}>
                                                                 <Text style={styles.dataTitle}>Wind Direction and Speed: </Text>
-                                                                <Text style={styles.dataContent}>{data.base.winddir_compass} {data.base.windspd_kmh}km/h</Text>
+                                                                <Text style={styles.dataContent}>{data.mid.winddir_compass} {data.mid.windspd_kmh}km/h</Text>
                                                             </View>
                                                             <View style={styles.dataRowContainer}>
                                                                 <Text style={styles.dataTitle}>Forecast: </Text>
-                                                                <Text style={styles.dataContent}>{data.base.wx_desc}</Text>
+                                                                <Text style={styles.dataContent}>{data.mid.wx_desc}</Text>
                                                             </View>
                                                         </>
                                                         :
-                                                        //mid mountain
-                                                        heightValue === "Mid"
-                                                            ?
-                                                            <>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Snow: </Text>
-                                                                    <Text style={styles.dataContent}>{data.mid.freshsnow_cm.toFixed(2)}cm</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Temperature: </Text>
-                                                                    <Text style={styles.dataContent}>{data.base.temp_c}°C</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Wind Direction and Speed: </Text>
-                                                                    <Text style={styles.dataContent}>{data.mid.winddir_compass} {data.mid.windspd_kmh}km/h</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Forecast: </Text>
-                                                                    <Text style={styles.dataContent}>{data.mid.wx_desc}</Text>
-                                                                </View>
-                                                            </>
-                                                            :
-                                                            //upper mountain
-                                                            <>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Snow: </Text>
-                                                                    <Text style={styles.dataContent}>{data.upper.freshsnow_cm.toFixed(2)}cm</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Temperature: </Text>
-                                                                    <Text style={styles.dataContent}>{data.upper.temp_c}°C</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Wind Direction and Speed: </Text>
-                                                                    <Text style={styles.dataContent}>{data.upper.winddir_compass} {data.upper.windspd_kmh}km/h</Text>
-                                                                </View>
-                                                                <View style={styles.dataRowContainer}>
-                                                                    <Text style={styles.dataTitle}>Forecast: </Text>
-                                                                    <Text style={styles.dataContent}>{data.upper.wx_desc}</Text>
-                                                                </View>
-                                                            </>
-                                                }
-                                            </Col>
-                                            <Col size={20}>
-                                                <View style={styles.imageContainer}>
-                                                    <Image style={styles.weatherIcon} source={weatherIcon[icon]} />
-                                                </View>
-                                            </Col>
-                                        </Grid>
-                                    </View>
-                                </>
-                            )
-                        })
-                        }
-                        <View style={styles.weatherUnlockedContainer}>
-                            <TouchableOpacity onPress={() => Linking.openURL('http://www.weatherunlocked.com/')}>
-                                <Text style={styles.weatherUnlockedText} >Weather Provided by</Text>
-                                <Image style={styles.weatherUnlockedImage} source={require('../assets/weatherUnlockedLogo.png')} />
-                            </TouchableOpacity>
-                        </View>
+                                                        //upper mountain
+                                                        <>
+                                                            <View style={styles.dataRowContainer}>
+                                                                <Text style={styles.dataTitle}>Snow: </Text>
+                                                                <Text style={styles.dataContent}>{data.upper.freshsnow_cm.toFixed(2)}cm</Text>
+                                                            </View>
+                                                            <View style={styles.dataRowContainer}>
+                                                                <Text style={styles.dataTitle}>Temperature: </Text>
+                                                                <Text style={styles.dataContent}>{data.upper.temp_c}°C</Text>
+                                                            </View>
+                                                            <View style={styles.dataRowContainer}>
+                                                                <Text style={styles.dataTitle}>Wind Direction and Speed: </Text>
+                                                                <Text style={styles.dataContent}>{data.upper.winddir_compass} {data.upper.windspd_kmh}km/h</Text>
+                                                            </View>
+                                                            <View style={styles.dataRowContainer}>
+                                                                <Text style={styles.dataTitle}>Forecast: </Text>
+                                                                <Text style={styles.dataContent}>{data.upper.wx_desc}</Text>
+                                                            </View>
+                                                        </>
+                                            }
+                                        </Col>
+                                        <Col size={20}>
+                                            <View style={styles.imageContainer}>
+                                                <Image style={styles.weatherIcon} source={weatherIcon[icon]} />
+                                            </View>
+                                        </Col>
+                                    </Grid>
+                                </View>
+                            </>
+                        )
+                    })
+                    }
+                    <View style={styles.weatherUnlockedContainer}>
+                        <TouchableOpacity onPress={() => Linking.openURL('http://www.weatherunlocked.com/')}>
+                            <Text style={styles.weatherUnlockedText} >Weather Provided by</Text>
+                            <Image style={styles.weatherUnlockedImage} source={require('../assets/weatherUnlockedLogo.png')} />
+                        </TouchableOpacity>
                     </View>
                 </ScrollView>
             </SafeAreaView>
