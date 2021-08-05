7672492971ad7a06cfcece463b196140a0136152
Alexander McKerrow <mckerrow4@gmail.com>
2021-08-05 12:53:08 +1200

    remove consolelog

--- screens/MountainScreen.js
+++ screens/MountainScreen.js
@@ -128,7 +128,6 @@ const MountainScreen = ({ route, navigation }) => {
                     </Grid>
                     {mountainForecastData.map((data) => {
                         const icon = data.upper.wx_icon.replace(".gif", "")
-                        console.log(icon)
                         return (
                             <View style={styles.dataContainer} key={data.date + data.time}>
                                 <Grid>
